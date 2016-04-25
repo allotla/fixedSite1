a4709997a9288f69659d1f1920f23e6b37e90595
Anthony Petiot <anthony.petiot@gmail.com>
2016-04-24 21:05:59 -0400

    Add animation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,28 @@
+var switchLike = function(event) {
+  event.preventDefault();
+  var a = $(this).find('a');
+  
+  if (/\s*Liked!\s*/.test(a.text())) {
+    a.text("Like this story!").closest('.news-item').removeClass('is-liked');
+  } else {
+    a.text("Liked!").closest('.news-item').addClass('is-liked');
+  }
+};
+
+var showForm = function(event) {
+  event.preventDefault();
+  $('.form').toggleClass('is-visible').find('.form-input').focus();
+}
+
+var hideForm = function(event) {
+  event.preventDefault();
+  $('.form').removeClass('is-visible');
+}
+
 $( document ).ready(function() {
-
-  // Get started!
-
+  
+  $('.news-meta').on('click', '.js-like', switchLike);
+  $('.js-add-link').on('click', showForm);
+  $('.form').on('submit', hideForm);
+  
 });
